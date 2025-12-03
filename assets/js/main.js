90cf0aa795e0793c733faf3ada330357084161eb
zam <ajamm999@gmail.com>
2025-12-03 22:43:19 +0700

    refactor: format code

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,7 @@ import "htmx.org"
 import Alpine from "alpinejs"
 import intersect from "@alpinejs/intersect"
 import * as m from "motion"
-
-import "./carousel.min.js"
-import "./dialog.min.js"
-import "./input.min.js"
-import "./label.min.js"
-import "./popover.min.js"
-import "./selectbox.min.js"
-import "./textarea.min.js"
-import "./sidebar.min.js"
+import.meta.glob('./*.min.js', { eager: true })
 
 window.Alpine = Alpine
 window.m = m
